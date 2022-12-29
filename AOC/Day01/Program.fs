9f6ee505863fc2c35fc0f2ae3d5a3dfe0ecb4f71
Ariel Rodriguez <69484552+rdrigezariel@users.noreply.github.com>
2022-12-28 23:04:55 -0500

    Day one solution

--- AOC/Day01/Program.fs
+++ AOC/Day01/Program.fs
@@ -1,2 +1,35 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open System.IO
+
+let processFile filePath =
+    let mutable elvesCalorie = List.empty
+    let mutable total = 0
+
+    File.ReadLines filePath
+    |> Seq.cache
+    |> Seq.iter (fun cal ->
+        match cal with
+        | "" ->
+            elvesCalorie <- total :: elvesCalorie
+            total <- 0
+        | _ -> total <- total + int (cal))
+
+    elvesCalorie |> Seq.sortDescending
+
+
+[<EntryPoint>]
+let main argv =
+    let inputFilePath = argv[0]
+
+    if not (File.Exists inputFilePath) then
+        printfn $"File not found: %s{inputFilePath}"
+        1
+    else
+        let elvesCalories = processFile inputFilePath
+        
+        // Part one solution
+        elvesCalories |> Seq.max |> printfn "Part one solution: %i"
+
+        // Part two solution
+        elvesCalories |> Seq.take 3 |> Seq.sum |> printfn "Part two solution: %i"
+
+        0
