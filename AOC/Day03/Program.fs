b5b648b8d464beb52e7a9c319219cfa53539db79
Ariel Rodriguez <arodriguez@enlighten.net>
2023-01-06 14:11:54 -0500

    WIP

---
+++ AOC/Day03/Program.fs
@@ -0,0 +1,12 @@
+﻿open System.IO
+
+let processInputFile =
+    File.ReadAllLines "input.txt"
+let validateInputs (inputs : string[]) =
+    Array.iter (fun line -> if (String.length line % 2) <> 0 then InvalidDataException() |> raise) inputs
+    inputs
+let distinguishCompartmentItems (inputs : string[]) =
+   Array.map (fun r ->
+       ("tesT", "Test")) inputs
+
+processInputFile |> validateInputs |> distinguishCompartmentItems |> Array.iter (fun (x, y) -> printfn $"x: %s{x} y: %s{y}")
